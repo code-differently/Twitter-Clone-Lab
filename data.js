de08502b8a8892e6fb8f3ff47e44b94920233a37
kburd <kalebmburd@gmail.com>
2020-12-04 16:04:47 -0500

    adding data

---
+++ data.js
@@ -0,0 +1,47 @@
+let data = [
+    {
+        name: "Kaleb Burd",
+        handle: "@kburd",
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim mollis sem, vel dignissim sem tempor eleifend. Vivamus et elit dapibus, porttitor nulla sed, blandit ex. -Kaleb",
+        time: "1:40 PM",
+        date: "16 Mar 20",
+        retweets: 6054,
+        likes: 27700
+    },
+    {
+        name: "Vlad Putin",
+        handle: "@noBotHere",
+        message: "Howdy all.....",
+        time: "4:51 PM",
+        date: "22 Jun 20",
+        retweets: 4,
+        likes: 27
+    },
+    {
+        name: "Jack Dorsey",
+        handle: "@daBoss",
+        message: "Twiter is the best social media on the internet, Zuckerberg is inferior",
+        time: "4:51 PM",
+        date: "22 Jun 20",
+        retweets: 4,
+        likes: 27
+    },
+    {
+        name: "Leon Jenkins",
+        handle: "@leyawn",
+        message: "Cool place.... looks uninhaited....",
+        time: "1:00 AM",
+        date: "1 May 89",
+        retweets: 123,
+        likes: 27334
+    },
+    {
+        name: "Mark Zuck",
+        handle: "@zuckman",
+        message: "This is hot garbage",
+        time: "3:59 PM",
+        date: "24 Dec 19",
+        retweets: 0,
+        likes: 0
+    }
+]
