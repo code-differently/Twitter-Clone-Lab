c27416a0322a53a2073de46c4f018b39a20ae592
Tajanae Boone <taj@Tajanaes-MacBook-Air.local>
2021-02-08 20:30:54 -0500

    clone of twitter

--- data.js
+++ data.js
@@ -1,14 +1,16 @@
 let data = [
     {
+        image: "https://images.unsplash.com/photo-1596550190729-1d9225e788dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
         name: "Kaleb Burd",
         handle: "@kburd",
-        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim mollis sem, vel dignissim sem tempor eleifend. Vivamus et elit dapibus, porttitor nulla sed, blandit ex. -Kaleb",
+        message: "Tweeting my life away -Kaleb",
         time: "1:40 PM",
         date: "16 Mar 20",
         retweets: 6054,
-        likes: 27700
+        likes: 27.7 + 'K'
     },
     {
+        image: "https://images.unsplash.com/photo-1596550190729-1d9225e788dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
         name: "Vlad Putin",
         handle: "@noBotHere",
         message: "Howdy all.....",
@@ -18,6 +20,7 @@ let data = [
         likes: 27
     },
     {
+        image: "https://images.unsplash.com/photo-1596550190729-1d9225e788dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
         name: "Jack Dorsey",
         handle: "@daBoss",
         message: "Twiter is the best social media on the internet, Zuckerberg is inferior",
@@ -27,6 +30,7 @@ let data = [
         likes: 27
     },
     {
+        image: "https://images.unsplash.com/photo-1596550190729-1d9225e788dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
         name: "Leon Jenkins",
         handle: "@leyawn",
         message: "Cool place.... looks uninhaited....",
@@ -36,6 +40,7 @@ let data = [
         likes: 27334
     },
     {
+        image: "https://images.unsplash.com/photo-1596550190729-1d9225e788dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
         name: "Mark Zuck",
         handle: "@zuckman",
         message: "This is hot garbage",
@@ -45,3 +50,40 @@ let data = [
         likes: 0
     }
 ]
+
+
+    for (let i = 0; i < data.length; i++) {
+        let image = data[i].image;
+        let name = data[i].name;
+        let handle = data[i].handle;
+        let message = data[i].message;
+        let time = data[i].time;
+        let date = data[i].date;
+        let retweets = data[i].retweets;
+        let likes = data[i].likes;
+        let tweet = document.createElement('div');
+        tweet.classList.add('tweet');name
+        tweet.innerHTML =
+`<div class="tweet__bio">
+<img src=${image} class="tweet__bio__picture"></div>
+<div class="tweet__bio__name">
+    <p>${name}</p>
+    <p class="at-name">${handle}</p>
+</div>
+</div>
+<div class="tweet__content">
+   ${message}
+</div>
+<div class="tweet__time-stamp">
+    <p class="at-name">${time} &#9679;</p>
+    <p class="at-name">${date} &#9679;</p>
+    <p class="blue-link">Twitter for iPhone</p>
+</div>
+<div class="tweet__social">
+    <p> <strong>${retweets}</strong> <div class="at-name">Retweets</div></p>
+    <p><strong>${likes}</strong> <div class="at-name">Likes</div></p>
+</div>`
+
+        
+document.getElementById('post').appendChild(tweet);
+}
